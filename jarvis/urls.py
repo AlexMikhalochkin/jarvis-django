0c68b794de8729375ed3d4e73a5df0716fbc6867
AlexMikhalochkin <alexmikhalochkin@gmail.com>
2021-03-21 17:38:48 +0300

    Removed auth and admin apps

--- jarvis/urls.py
+++ jarvis/urls.py
@@ -1,7 +1,5 @@
-from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('smartthings/', include('jarvis_api.urls')),
-    path('admin/', admin.site.urls),
+    path('smartthings/', include('jarvis_api.urls'))
 ]
